797da2c2396d5a159b216f7655608ddfc1015c88
Brook Mesfin <brookmesfin101@gmail.com>
2018-02-12 22:30:11 -0600

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,14 @@ socket.on('connect', function () {
     console.log("Connected to server");
 });
 
-socket.emit("createMessage", {
-    to: "mesfin@example.com",
-    text: "Do your homework"
-});
+socket.on("join", function () {
+    console.log("Welcome to chat app");
+})
+
+// socket.emit("createMessage", {
+//     to: "mesfin@example.com",
+//     text: "Do your homework"
+// });
 
 socket.on("disconnect", function () {
     console.log("Disconnected from server");
