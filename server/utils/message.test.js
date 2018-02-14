fea7d7530e786dc9927d6c473683771fc8863086
Brook Mesfin <brookmesfin101@gmail.com>
2018-02-13 22:38:04 -0600

    Add geolocation support via gelocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate corrrect message object', () => {
@@ -10,5 +10,21 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        //from property is correct
+        //created at is a number
+        //url is correct given lat and long
+        var from = 'Brook';
+        var lat = 1;
+        var lng = 1;
+        var url = 'https://www.google.com/maps?q=1,1';
+        var locationMessage = generateLocationMessage(from, lat, lng);
+
+        expect(locationMessage.createdAt).toBeA('number');
+        expect(locationMessage.url).toBe(url);        
     });
 });
